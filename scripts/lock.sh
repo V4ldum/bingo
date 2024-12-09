5de5bb85d268bd13cfe5d01ec565de8682ef2bcf
V4ldum <56064897+V4ldum@users.noreply.github.com>
2024-12-09 14:37:20 +0100

    fix lock.sh

--- scripts/lock.sh
+++ scripts/lock.sh
@@ -2,4 +2,4 @@
 
 LOCK_FILE="/tmp/bingo.lock"
 HOME="/home/bingo"
-flock -n $LOCK_FILE "bash $HOME/bingo/scripts/deploy_if_changed.sh" >> "$HOME/logs/bingo-deploy.log" 2>&1
+flock -n $LOCK_FILE -c "bash $HOME/bingo/scripts/deploy_if_changed.sh" >> "$HOME/logs/bingo-deploy.log" 2>&1
